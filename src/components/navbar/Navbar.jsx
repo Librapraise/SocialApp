ed5fcf0766081fc0c64a3131aa2fe21756cbb991
Librapraise <alabipraise26@gmail.com>
2025-08-04 09:43:25 +0100

    updated app

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./navbar.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
@@ -8,16 +8,21 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkMode";
 import { AuthContext } from "../../context/authContext";
 
-
-
 const NavBar = () => {
-
     const { toggle, darkMode } = useContext(DarkModeContext);
-    const { currentUser } = useContext(AuthContext);
+    const { currentUser, logout } = useContext(AuthContext);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleLogout = () => {
+        logout();
+        setShowDropdown(false);
+    };
 
     return (
         <div className="navbar">
@@ -37,16 +42,42 @@ const NavBar = () => {
                 <PersonOutlinedIcon />
                 <EmailOutlinedIcon />
                 <NotificationsOutlinedIcon />
-                <div className="user">
+                <div 
+                    className="user"
+                    onMouseEnter={() => setShowDropdown(true)}
+                    onMouseLeave={() => setShowDropdown(false)}
+                >
                     <img 
-                        src={ currentUser.profilePic }
-                        alt="img" 
+                        src={currentUser.profilePic}
+                        alt="Profile" 
                     />
-                    <span>{ currentUser.username }</span>
+                    <span>{currentUser.username}</span>
+                    <ExpandMoreIcon className="dropdown-arrow" />
+                    
+                    {showDropdown && (
+                        <div className="dropdown-menu">
+                            <Link 
+                                to={`/profile/${currentUser.id}`} 
+                                className="dropdown-item"
+                                onClick={() => setShowDropdown(false)}
+                            >
+                                <PersonOutlinedIcon />
+                                <span>My Profile</span>
+                            </Link>
+                            <div className="dropdown-divider"></div>
+                            <div 
+                                className="dropdown-item logout-item" 
+                                onClick={handleLogout}
+                            >
+                                <LogoutOutlinedIcon />
+                                <span>Logout</span>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default NavBar;
